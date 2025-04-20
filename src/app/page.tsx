3e6b612cc47f4b6516a16b56dfa8e99815288dc0
Suhaibu Yehuza <suhai.yehuza@gmail.com>
2025-04-20 09:55:44 -0400

    Update landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,33 @@
+"use client";
+
 import Image from "next/image";
+import { useUser } from "@clerk/nextjs";
 
 export default function Home() {
+  const { isLoaded, isSignedIn } = useUser();
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1 className="text-2xl font-bold text-center">🚧 ... Work in progress ... 🚧</h1>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center justify-center text-center">
+        <h1 className="text-2xl font-bold">
+          {isLoaded && isSignedIn ? "You are now logged in" : "🚧 ... Work in progress ... 🚧"}
+        </h1>
+        <div className="flex gap-4 items-center justify-center">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="https://game-diary-git-syehuza-dev-suhais-projects-33a81a2a.vercel.app"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <Image
-              src="/gamelog.svg"
-              alt="logomark"
-              width={20}
-              height={20}
-            />
-            Visit our staging site for our WIP
+            {!isSignedIn && (
+              <Image
+                src="/gamelog.svg"
+                alt="logomark"
+                width={20}
+                height={20}
+              />
+            )}
+            {isLoaded && isSignedIn ? "🚧 ... You will be able to log games soon ... 🚧" : "Visit our staging site for our WIP"}
           </a>
         </div>
       </main>
