c9c72fae6a55979fef57bc6e335c585097fd7555
Suhaibu Yehuza <suhai.yehuza@gmail.com>
2025-04-22 14:32:48 -0400

    fix export issue

--- src/hooks/use-mounted.ts
+++ src/hooks/use-mounted.ts
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 
-export function useMounted() {
+export default function useMounted() {
   const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
